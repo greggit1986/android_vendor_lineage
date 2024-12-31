d1d35fa79fb7a5f5747b280cd3924d065f9e10ce
HeCheng Yu <kde-yyds@qq.com>
2024-12-31 23:54:44 +0900

    crdroid: Disable default frame rate limit for games
    * Android 15 limits refresh rate to 60hz for games unless we enable
      the developer option 'Disable default frame rate for games' manually.
    * Adding this prop enables it by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -23,6 +23,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
+# Disable default frame rate limit for games
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=true
+
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
